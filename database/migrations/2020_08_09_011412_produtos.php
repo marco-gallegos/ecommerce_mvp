9521adbb3f73029d86e2b790c3efea7e9b71b489
Marco A. Gallegos <marco-allegos>
2020-08-08 20:20:31 -0500

    [init] inicial
     estructura inicial en conjunto con la idea principal del proyecto
     ecomerce en DB realizada con workbench

---
+++ database/migrations/2020_08_09_011412_produtos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Produtos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nombre', 200);
+            $table->float('precio');
+            $table->integer('existencia')->default(0);
+            $table->string('medida', 200);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
